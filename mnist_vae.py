c515ce19dfc56e7f844b4f8a340140f68b9f9f6e
Nesar Ramachandra <nayaknesar@gmail.com>
2017-12-25 19:08:27 -0600

    initial commit

--- mnist_vae.py
+++ mnist_vae.py
@@ -50,3 +50,41 @@ outputs = decoder_out(h_p)
 # -------------------------------------------------------------
 
 
+# Overall VAE model, for reconstruction and training
+vae = Model(inputs, outputs)
+
+# Encoder model, to encode input into latent variable
+# We use the mean as the output as it is the center point, the representative of the gaussian
+encoder = Model(inputs, mu)
+
+# Generator model, generate new data given latent variable z
+d_in = Input(shape=(n_z,))
+d_h = decoder_hidden(d_in)
+d_out = decoder_out(d_h)
+decoder = Model(d_in, d_out)
+
+
+# -------------------------------------------------------------
+
+
+def vae_loss(y_true, y_pred):
+    """ Calculate loss = reconstruction loss + KL loss for each data in minibatch """
+    # E[log P(X|z)]
+    recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
+    # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
+    kl = 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
+
+    return recon + kl
+
+# -------------------------------------------------------------
+#TRAIN
+vae.compile(optimizer='adam', loss=vae_loss)
+vae.fit(X_train, X_train, batch_size=m, nb_epoch=n_epoch)
+
+# -------------------------------------------------------------
+
+
+
+
+
+
