6be45376307c45ad775101ef1f4e32dbf81ad913
Nesar Ramachandra <nayaknesar@gmail.com>
2018-01-03 09:48:31 -0600

    Pk

--- keras_vae_pk.py
+++ keras_vae_pk.py
@@ -16,12 +16,12 @@ from keras.models import Model
 from keras import backend as K
 from keras import metrics
 
-batch_size = 100
+batch_size = 256
 original_dim = 351 # mnist ~ 784
-latent_dim = 7
-intermediate_dim = 128 # mnist ~ 256
-epochs = 20 #110 #50
-epsilon_std = 1.0
+latent_dim = 2
+intermediate_dim = 64 # mnist ~ 256
+epochs = 4 #110 #50
+epsilon_std = 1.0 # 1.0
 
 
 x = Input(shape=(original_dim,))
@@ -94,15 +94,15 @@ print(y_test.shape, 'test sequences')
 
 
 
-x_train = x_train.astype('float32') #/ 255.
-x_test = x_test.astype('float32') #/ 255.
+x_train = x_train.astype('float32')/5. #/ 255.
+x_test = x_test.astype('float32')/5. #/ 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
 
 # ------------------------------------------------------------------------------
 #
-plt.figure(1034)
+plt.figure(34)
 plt.plot(np.load('../Pk_data/Pk.npy')[100])
 plt.plot(x_train[100], 'x')
 plt.yscale('log')
@@ -117,7 +117,7 @@ encoder = Model(x, z_mean)
 # display a 2D plot of the digit classes in the latent space
 x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
 plt.figure(figsize=(6, 6))
-plt.scatter(x_test_encoded[:, 1], x_test_encoded[:, 5], c=y_test[:,1])
+plt.scatter(x_test_encoded[:, 1], x_test_encoded[:, 0], c=y_test[:,1])
 plt.colorbar()
 plt.show()
 
@@ -133,25 +133,26 @@ digit_size = original_dim
 figure = np.zeros((digit_size * n, digit_size * n))
 # linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
 # to produce values of the latent variables z, since the prior of the latent space is Gaussian
+
 grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
 grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
 # for i, yi in enumerate(grid_x):
 #     for j, xi in enumerate(grid_y):
 
-z_sample = np.array([np.random.uniform(size = 7)])
+z_sample = np.array([np.random.uniform(size = latent_dim)])
 # z_sample = np.expand_dims(z_sample, axis=1)
 x_decoded = generator.predict(z_sample)
-plt.figure(30, figsize=(8,6))
-plt.plot(x_decoded[0], 'b', alpha = 0.5)
+plt.figure(91, figsize=(8,6))
+plt.plot(x_decoded[0], 'bx', alpha = 0.5)
 
 
 
 PlotSample = True
 if PlotSample:
     for i in range(10):
-        plt.figure(i, figsize=(8,6) )
-        plt.plot(x_test[i,:], 'r', alpha = 0.8)
+        plt.figure(91, figsize=(8,6))
+        plt.plot(x_test[i,:], 'r', alpha = 0.7)
 
     plt.show()
 
