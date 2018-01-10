d101b3f46ef375bc81225a54b26309dd1405f0f9
Nesar Ramachandra <nayaknesar@gmail.com>
2018-01-09 23:29:15 -0600

    CMB - from CAMB

---
+++ Cl_CAMB.py
@@ -0,0 +1,98 @@
+import sys, platform, os
+from matplotlib import pyplot as plt
+import numpy as np
+print('Using CAMB installed at %s'%(os.path.realpath(os.path.join(os.getcwd(),'..'))))
+#uncomment this if you are running remotely and want to keep in synch with repo changes
+#if platform.system()!='Windows':
+#    !cd $HOME/git/camb; git pull github master; git log -1
+sys.path.insert(0,os.path.realpath(os.path.join(os.getcwd(),'..')))
+import camb
+from camb import model, initialpower
+print('CAMB version: %s '%camb.__version__)
+
+
+#Set up a new set of parameters for CAMB
+pars = camb.CAMBparams()
+#This function sets up CosmoMC-like settings, with one massive neutrino and helium set using BBN consistency
+pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122, mnu=0.06, omk=0, tau=0.06)
+pars.InitPower.set_params(ns=0.965, r=0)
+pars.set_for_lmax(2500, lens_potential_accuracy=0);
+
+#calculate results for these parameters
+results = camb.get_results(pars)
+
+#get dictionary of CAMB power spectra
+powers =results.get_cmb_power_spectra(pars, CMB_unit='muK')
+for name in powers: print(name)
+
+#plot the total lensed CMB power spectra versus unlensed, and fractional difference
+totCL=powers['total']
+unlensedCL=powers['unlensed_scalar']
+print(totCL.shape)
+#Python CL arrays are all zero based (starting at L=0), Note L=0,1 entries will be zero by default.
+#The different CL are always in the order TT, EE, BB, TE (with BB=0 for unlensed scalar results).
+ls = np.arange(totCL.shape[0])
+fig, ax = plt.subplots(2,2, figsize = (12,12))
+ax[0,0].plot(ls,totCL[:,0], color='k')
+ax[0,0].plot(ls,unlensedCL[:,0], color='r')
+ax[0,0].set_title('TT')
+ax[0,1].plot(ls[2:], 1-unlensedCL[2:,0]/totCL[2:,0]);
+ax[0,1].set_title(r'$\Delta TT$')
+ax[1,0].plot(ls,totCL[:,1], color='k')
+ax[1,0].plot(ls,unlensedCL[:,1], color='r')
+ax[1,0].set_title(r'$EE$')
+ax[1,1].plot(ls,totCL[:,3], color='k')
+ax[1,1].plot(ls,unlensedCL[:,3], color='r')
+ax[1,1].set_title(r'$TE$');
+for ax in ax.reshape(-1): ax.set_xlim([2,2500]);
+
+
+#-------------------------------------------------------------------------------
+
+
+
+#You can calculate spectra for different primordial power spectra without recalculating everything
+#for example, let's plot the BB spectra as a function of r
+pars.WantTensors = True
+results = camb.get_transfer_functions(pars)
+lmax=2000
+rs = np.linspace(0,0.2,6)
+for r in rs:
+    inflation_params = initialpower.InitialPowerParams()
+    inflation_params.set_params(ns=0.96, r=r)
+    results.power_spectra_from_transfer(inflation_params)
+    cl = results.get_total_cls(lmax, CMB_unit='muK')
+    plt.loglog(np.arange(lmax+1),cl[:,2])
+plt.xlim([2,lmax])
+plt.legend(["$r = %s$"%r for r in  rs], loc='lower right');
+plt.ylabel(r'$\ell(\ell+1)C_\ell^{BB}/ (2\pi \mu{\rm K}^2)$')
+plt.xlabel(r'$\ell$');
+
+#Now get matter power spectra and sigma8 at redshift 0 and 0.8
+pars = camb.CAMBparams()
+pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122)
+pars.set_dark_energy() #re-set defaults
+pars.InitPower.set_params(ns=0.965)
+#Not non-linear corrections couples to smaller scales than you want
+pars.set_matter_power(redshifts=[0., 0.8], kmax=2.0)
+
+#Linear spectra
+pars.NonLinear = model.NonLinear_none
+results = camb.get_results(pars)
+kh, z, pk = results.get_matter_power_spectrum(minkh=1e-4, maxkh=1, npoints = 200)
+s8 = np.array(results.get_sigma8())
+
+#Non-Linear spectra (Halofit)
+pars.NonLinear = model.NonLinear_both
+results.calc_power_spectra(pars)
+kh_nonlin, z_nonlin, pk_nonlin = results.get_matter_power_spectrum(minkh=1e-4, maxkh=1, npoints = 200)
+
+
+print(results.get_sigma8())
+
+for i, (redshift, line) in enumerate(zip(z,['-','--'])):
+    plt.loglog(kh, pk[i,:], color='k', ls = line)
+    plt.loglog(kh_nonlin, pk_nonlin[i,:], color='r', ls = line)
+plt.xlabel('k/h Mpc');
+plt.legend(['linear','non-linear'], loc='lower left');
+plt.title('Matter power at z=%s and z= %s'%tuple(z));
