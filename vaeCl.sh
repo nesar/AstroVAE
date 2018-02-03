d12ad7cd0e33d136afcfd272cb710cb89d6e3f37
Nesar Ramachandra <nayaknesar@gmail.com>
2018-02-02 19:26:18 -0600

    phoenix push: CuBLAS issue

--- vaeCl.sh
+++ vaeCl.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-#SBATCH -n 1 -N 1
+#SBATCH -p cp100
 
 echo [$SECONDS] setting up environment
 
 export PATH="/cosmo/software/anaconda3/bin:$PATH"
 export PYTHONPATH=/cosmo/software/anaconda3/lib/python3.6/site-packages/:$HOME/.conda/envs/nes_keras/lib/python3.6/site-packages/
 
-python Cl_denoiseVAE.py
-python Cl_VAEpredict.py
+#python Cl_denoiseVAE.py
+#python Cl_VAEpredict.py
+
+srun -p cp100 python Cl_denoiseVAE.py
+srun -p cp100 python Cl_VAEpredict.py
 
 echo [$SECONDS] job completed
