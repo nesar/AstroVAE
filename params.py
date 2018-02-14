442d3fa83b23f76b9efeb300256dea9c1e3d727e
Nesar Ramachandra <nayaknesar@gmail.com>
2018-02-14 10:20:27 -0600

    new phoenix

--- params.py
+++ params.py
@@ -4,19 +4,19 @@ original_dim = 2549#/2 +1  #2551 # mnist ~ 784
 intermediate_dim2 = 1024#/2 #
 intermediate_dim1 = 512#/2 #
 intermediate_dim = 256#/2 #
-latent_dim = 16
+latent_dim = 8
 
 totalFiles = 512
 TestFiles = 32 #128
 
 batch_size = 8
-num_epochs = 100 #110 #50
+num_epochs = 200 #110 #50
 epsilon_mean = 0.0 # 1.0
 epsilon_std = 1.0 # 1.0
-learning_rate = 1e-3
+learning_rate = 0.001
 decay_rate = 0.0
 
-noise_factor = 0.00 # 0.0 necessary
+noise_factor = 0.1 # 0.0 necessary
 
 ######################## I/O #################################
 
