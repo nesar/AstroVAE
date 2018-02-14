d5d03016925f37d8c064002425f70a552cb33c7f
Nesar Ramachandra <nayaknesar@gmail.com>
2018-02-14 10:44:40 -0600

    laptop update

--- params.py
+++ params.py
@@ -10,13 +10,13 @@ totalFiles = 512
 TestFiles = 32 #128
 
 batch_size = 8
-num_epochs = 200 #110 #50
+num_epochs = 20 #110 #50
 epsilon_mean = 0.0 # 1.0
 epsilon_std = 1.0 # 1.0
 learning_rate = 0.001
 decay_rate = 0.0
 
-noise_factor = 0.1 # 0.0 necessary
+noise_factor = 0.0 # 0.0 necessary
 
 ######################## I/O #################################
 
