b8610888631c05d673dc6c67064aed8ba3150007
Nesar Ramachandra <nayaknesar@gmail.com>
2018-02-21 15:20:15 -0600

    analysis without sigma8

--- params.py
+++ params.py
@@ -4,16 +4,16 @@ original_dim = 2549#/2 +1  #2551 # mnist ~ 784
 intermediate_dim2 = 1024#/2 #
 intermediate_dim1 = 512#/2 #
 intermediate_dim = 256#/2 #
-latent_dim = 12
+latent_dim = 16
 
 totalFiles = 256
 TestFiles = 16 #128
 
-batch_size = 8
-num_epochs = 200 #110 #50
+batch_size = 4
+num_epochs = 100 #110 #50
 epsilon_mean = 0.0 # 1.0
 epsilon_std = 0.01 ## original = 1.0, smaller the better 1e-4
-learning_rate =  1e-5
+learning_rate =  1e-4
 decay_rate = 1.0
 
 noise_factor = 0.0 # 0.0 necessary
