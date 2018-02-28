21197a71182f7b298cde5d39b4246d7c2ecda7b2
Nesar Ramachandra <nayaknesar@gmail.com>
2018-02-27 20:42:32 -0600

    5 layers

--- Cl_denoiseP4.py
+++ Cl_denoiseP4.py
@@ -33,6 +33,7 @@ K.set_floatx('float32')
 ###################### PARAMETERS ##############################
 
 original_dim = params.original_dim # 2549
+intermediate_dim3 = params.intermediate_dim3 # 1024
 intermediate_dim2 = params.intermediate_dim2 # 1024
 intermediate_dim1 = params.intermediate_dim1 # 512
 intermediate_dim0 = params.intermediate_dim0 # 256
@@ -67,7 +68,8 @@ fileOut = params.fileOut
 
 # Q(z|X) -- encoder
 inputs = Input(shape=(original_dim,))
-h_q2 = Dense(intermediate_dim2, activation='relu')(inputs) # ADDED intermediate layer
+h_q3 = Dense(intermediate_dim3, activation='relu')(inputs) # ADDED intermediate layer
+h_q2 = Dense(intermediate_dim2, activation='relu')(h_q3) # ADDED intermediate layer
 h_q1 = Dense(intermediate_dim1, activation='relu')(h_q2) # ADDED intermediate layer
 h_q0 = Dense(intermediate_dim0, activation='relu')(h_q1) # ADDED intermediate layer
 h_q = Dense(intermediate_dim, activation='relu')(h_q0)
@@ -93,6 +95,7 @@ decoder_hidden0 = Dense(intermediate_dim, activation='relu') # ADDED intermediat
 decoder_hidden1 = Dense(intermediate_dim0, activation='relu') # ADDED intermediate layer
 decoder_hidden2 = Dense(intermediate_dim1, activation='relu') # ADDED intermediate layer
 decoder_hidden3 = Dense(intermediate_dim2, activation='relu') # ADDED intermediate layer
+decoder_hidden4 = Dense(intermediate_dim3, activation='relu') # ADDED intermediate layer
 decoder_out = Dense(original_dim, activation='sigmoid')
 
 h_p0 = decoder_hidden(z)
@@ -100,7 +103,8 @@ h_p1 = decoder_hidden0(h_p0) # ADDED intermediate layer
 h_p2 = decoder_hidden1(h_p1) # ADDED intermediate layer
 h_p3 = decoder_hidden2(h_p2) # ADDED intermediate layer
 h_p4 = decoder_hidden3(h_p3) # ADDED intermediate layer
-outputs = decoder_out(h_p4)
+h_p5 = decoder_hidden4(h_p4) # ADDED intermediate layer
+outputs = decoder_out(h_p5)
 
 # ----------------------------------------------------------------------------
 
@@ -128,7 +132,8 @@ _h0_decoded = decoder_hidden0(_h_decoded)    ## ADDED layer_1
 _h1_decoded = decoder_hidden1(_h0_decoded)    ## ADDED layer_1
 _h2_decoded = decoder_hidden2(_h1_decoded)    ## ADDED ---
 _h3_decoded = decoder_hidden3(_h2_decoded)    ## ADDED --- should replicate decoder arch
-_x_decoded_mean = decoder_out(_h3_decoded)
+_h4_decoded = decoder_hidden4(_h3_decoded)    ## ADDED --- should replicate decoder arch
+_x_decoded_mean = decoder_out(_h4_decoded)
 decoder = Model(decoder_input, _x_decoded_mean)
 
 
