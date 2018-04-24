21002954bd213069c063319e84df7a6b75fc723a
Nesar Ramachandra <nayaknesar@gmail.com>
2018-04-24 16:09:58 -0500

    mcmc update

--- mcmc_GP.py
+++ mcmc_GP.py
@@ -18,11 +18,14 @@ m_true = -0.9594
 b_true = 4.294
 f_true = 0.534
 
+
+
+########## REAL DATA with ERRORS #############################
 # Generate some synthetic data from the model.
 N = 50
 x = np.sort(10*np.random.rand(N))
 yerr = 0.1+0.5*np.random.rand(N)
-y = m_true*x+b_true
+y = m_true*x+b_true**2
 y += np.abs(f_true*y) * np.random.randn(N)
 y += yerr * np.random.randn(N)
 
