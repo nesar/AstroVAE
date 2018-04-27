757fbd0ef1449e6236dc61f47b7fa43d9a782b76
Nesar Ramachandra <nayaknesar@gmail.com>
2018-04-27 15:44:20 -0500

    mcmc 5 para

--- mcmc_5para.py
+++ mcmc_5para.py
@@ -210,19 +210,28 @@ x_decoded = GPfit(computedGP, y_test[10])
 
 
 #### parameters that define the MCMC
-ndim = 2
+
+ndim = 5
 nwalkers = 50  # 500
-nrun_burn = 100  # 300
-nrun = 700  # 700
+nrun_burn = 10  # 300
+nrun = 70  # 700
 
 #### Cosmological Parameters ########################################
+
+# OmegaM = np.linspace(0.12, 0.155, totalFiles)
+# Omegab = np.linspace(0.0215, 0.0235, totalFiles)
+# sigma8 = np.linspace(0.7, 0.9, totalFiles)
+# h = np.linspace(0.55, 0.85, totalFiles)
+# ns = np.linspace(0.85, 1.05, totalFiles)
 
 #### Order of parameters: ['Omega_m', 'Omega_b', 'sigma_8', 'h', 'n_s']
 #        [label, true, min, max]
 
-param1 = ["$\Omega_c h^2$", 0.121, 0.12, 0.155]
-# param2 = ["$\sigma_8$", 0.81, 0.7, 0.9]
-param2 = ["$h$", 0.67, 0.55, 0.85]
+param1 = ["$\Omega_c h^2$", 0.121, 0.12, 0.155] # Actual 0.119
+param2 = ["$\Omega_b h^2$", 0.022, 0.0215, 0.0235]
+param3 = ["$\sigma_8$", 0.81, 0.7, 0.9]
+param4 = ["$h$", 0.67, 0.55, 0.85]
+param5 = ["$n_s$", 0.9667, 0.85, 1.05]
 
 ## Make sure the changes are made in log prior definition too. Variable: new_params
 
@@ -234,42 +243,39 @@ param2 = ["$h$", 0.67, 0.55, 0.85]
 
 # Initialize the chain
 # Choice 1: chain uniformly distributed in the range of the parameters
-pos_min = np.array([param1[2], param2[2]])
-pos_max = np.array([param1[3], param2[3]])
+pos_min = np.array([param1[2], param2[2], param3[2], param4[2], param5[2]])
+pos_max = np.array([param1[3], param2[3], param3[3], param4[3], param5[3]])
 psize = pos_max - pos_min
 pos = [pos_min + psize * np.random.rand(ndim) for i in range(nwalkers)]
 
 # Visualize the initialization
 import corner
 
-fig = corner.corner(pos, labels=[param1[0], param2[0]], range=[[param1[2], param1[3]], [param2[2],
-                                                                                        param2[3]]],
-                    truths=[param1[1], param2[1]])
+fig = corner.corner(pos, labels=[param1[0], param2[0], param3[0], param4[0], param5[0]],
+                    range=[[param1[2],param1[3]], [param2[2], param2[3]], [param3[2],param3[3]],
+                    [param4[2],param4[3]], [param5[2],param5[3]]],
+                    truths=[param1[1], param2[1], param3[1], param4[1], param5[1]])
 fig.set_size_inches(10, 10)
 
 
 ######### MCMC #######################
 
-# OmegaM = np.linspace(0.12, 0.155, totalFiles)
-# Omegab = np.linspace(0.0215, 0.0235, totalFiles)
-# sigma8 = np.linspace(0.7, 0.9, totalFiles)
-# h = np.linspace(0.55, 0.85, totalFiles)
-# ns = np.linspace(0.85, 1.05, totalFiles)
-
+## Sample implementation : http://eso-python.github.io/ESOPythonTutorials/ESOPythonDemoDay8_MCMC_with_emcee.html
 
 def lnprior(theta):
-    p1, p2 = theta
+    p1, p2, p3, p4, p5 = theta
     # if 0.12 < p1 < 0.155 and 0.7 < p2 < 0.9:
-    if param1[2] < p1 < param1[3] and param2[2] < p2 < param2[3]:
+    if param1[2] < p1 < param1[3] and param2[2] < p2 < param2[3] and param3[2] < p3 < param3[3] \
+            and param4[2] < p4 < param4[3] and param5[2] < p5 < param5[3]:
         return 0.0
     return -np.inf
 
 
 def lnlike(theta, x, y, yerr):
-    p1, p2 = theta
+    p1, p2, p3, p4, p5 = theta
     # new_params = np.array([p1, 0.0225, p2 , 0.74, 0.9])
 
-    new_params = np.array([p1, 0.0225, 0.1201, p2, 0.9])
+    new_params = np.array([p1, p2, p3, p4, p5])
     model = GPfit(computedGP, new_params)[28:2507]
 
     return -0.5 * (np.sum(((y - model) / yerr) ** 2.))
@@ -308,10 +314,14 @@ print('mcmc time:', time1 - time0)
 samples = sampler.flatchain
 samples.shape
 
+
+###########################################################################
 samples_plot = sampler.chain[:, :].reshape((-1, ndim))
 
-fig = corner.corner(samples_plot, labels=[param1[0], param2[0]],
-                    truths=[param1[1], param2[1]])
+fig = corner.corner(samples_plot, labels=[param1[0], param2[0], param3[0], param4[0], param5[0]],
+                    range=[[param1[2],param1[3]], [param2[2], param2[3]], [param3[2],param3[3]],
+                    [param4[2],param4[3]], [param5[2],param5[3]]],
+                    truths=[param1[1], param2[1], param3[1], param4[1], param5[1]])
 fig.savefig('corner_' + fileOut + '.png')
 
 np.savetxt(DataDir + 'Sampler_mcmc_ndim' + str(ndim) + '_nwalk' + str(nwalkers) + '_run' + str(
@@ -320,24 +330,27 @@ np.savetxt(DataDir + 'Sampler_mcmc_ndim' + str(ndim) + '_nwalk' + str(nwalkers) 
 ####### FINAL PARAMETER ESTIMATES #######################################
 
 # samples = np.exp(samples)
-p1_mcmc, p2_mcmc = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
+p1_mcmc, p2_mcmc, p3_mcmc, p4_mcmc, p5_mcmc = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
                        zip(*np.percentile(samples, [16, 50, 84], axis=0)))
-print('mcmc results:', p1_mcmc[0], p2_mcmc[0])
+print('mcmc results:', p1_mcmc[0], p2_mcmc[0], p3_mcmc[0], p4_mcmc[0], p5_mcmc[0])
 
 ####### CORNER PLOT ESTIMATES #######################################
 
 
 import corner
 
-fig = corner.corner(samples_plot, labels=[param1[0], param2[0]],
-                    truths=[param1[1], param2[1]])
+fig = corner.corner(samples_plot, labels=[param1[0], param2[0], param3[0], param4[0], param5[0]],
+                    range=[[param1[2],param1[3]], [param2[2], param2[3]], [param3[2],param3[3]],
+                    [param4[2],param4[3]], [param5[2],param5[3]]],
+                    truths=[param1[1], param2[1], param3[1], param4[1], param5[1]])
 fig.savefig('corner_' + fileOut + '.png')
 
 import pygtc
 
-fig = pygtc.plotGTC(samples_plot, paramNames=[param1[0], param2[0]], truths=[param1[1],
-                                                                             param2[1]],
-                    figureSize='MNRAS_page')
+fig = pygtc.plotGTC(samples_plot, paramNames=[param1[0], param2[0], param3[0], param4[0], param5[0]],
+                    truths=[param1[1], param2[1], param3[1], param4[1], param5[1]], figureSize='MNRAS_page')
+
+
 fig.savefig('pygtc_' + fileOut + '.png')
 
 ####### FINAL PARAMETER ESTIMATES #######################################
@@ -356,12 +369,26 @@ fig.savefig('pygtc_' + fileOut + '.png')
 
 
 fig = plt.figure(13214)
-ax1 = fig.add_subplot(1, 1, 1)
-ax2 = fig.add_subplot(2, 1, 1)
-# ax3 = fig.add_subplot(3, 1, 3)
+plt.xlabel('steps')
+ax1 = fig.add_subplot(5, 1, 1)
+ax2 = fig.add_subplot(5, 1, 2)
+ax3 = fig.add_subplot(5, 1, 3)
+ax4 = fig.add_subplot(5, 1, 4)
+ax5 = fig.add_subplot(5, 1, 5)
 
 ax1.plot(np.arange(nrun - 10), sampler.chain[:, 10:, 0].T)
+ax1.text(0.9, 0.9, param1[0], horizontalalignment='center', verticalalignment='center',
+         transform = ax1.transAxes, fontsize = 20)
 ax2.plot(np.arange(nrun - 10), sampler.chain[:, 10:, 1].T)
-# ax3.plot(np.arange(nrun), sampler.chain[:,:, 2].T)
-
+ax2.text(0.9, 0.9, param2[0], horizontalalignment='center', verticalalignment='center',
+         transform = ax2.transAxes, fontsize = 20)
+ax3.plot(np.arange(nrun - 10), sampler.chain[:, 10:, 2].T)
+ax3.text(0.9, 0.9, param3[0], horizontalalignment='center', verticalalignment='center',
+         transform = ax3.transAxes, fontsize = 20)
+ax4.plot(np.arange(nrun - 10), sampler.chain[:, 10:, 3].T)
+ax4.text(0.9, 0.9, param4[0], horizontalalignment='center', verticalalignment='center',
+         transform = ax4.transAxes, fontsize = 20)
+ax5.plot(np.arange(nrun - 10), sampler.chain[:, 10:, 4].T)
+ax5.text(0.9, 0.9, param5[0], horizontalalignment='center', verticalalignment='center',
+         transform = ax5.transAxes, fontsize = 20)
 plt.show()
