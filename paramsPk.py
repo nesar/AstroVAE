fb9ea17beda398ff01370c75caf94a6fa5acd62d
Nesar Ramachandra <nayaknesar@gmail.com>
2018-07-09 19:45:04 -0500

    Pk: dropout

--- paramsPk.py
+++ paramsPk.py
@@ -1,8 +1,8 @@
 ######################## PARAMETERS ##########################
 
 original_dim = 256#/2 +1  #2551 # mnist ~ 784
-#intermediate_dim3 = 1600
-intermediate_dim2 = 1024
+intermediate_dim3 = 1024
+intermediate_dim2 = 512
 intermediate_dim1 = 256
 intermediate_dim0 = 128
 intermediate_dim = 64
@@ -14,10 +14,10 @@ num_test = 16
 num_para = 5
 
 batch_size = 16
-num_epochs =  10000 # 20  #200 # 7500 # 200  #110 #50
+num_epochs =  20000 # 20  #200 # 7500 # 200  #110 #50
 epsilon_mean = 0.0 # 1.0
 epsilon_std = 1e-6#1e-6 ## original = 1.0, smaller the better 1e-4
-learning_rate =  1e-5
+learning_rate = 1e-5
 decay_rate = 1.0
 
 noise_factor = 0.0 # 0.0 necessary
@@ -34,3 +34,32 @@ fileOut = 'P'+str(num_para)+'Model_tot' + str(num_train) + '_batch' + str(batch_
     num_epochs)
 
 ##############################################################
+
+
+# ######################## PARAMETERS ##########################
+# 7.7 percent  - have to train again
+
+#
+# original_dim = 256#/2 +1  #2551 # mnist ~ 784
+# #intermediate_dim3 = 1600
+# intermediate_dim2 = 512
+# intermediate_dim1 = 256
+# intermediate_dim0 = 128
+# intermediate_dim = 64
+# latent_dim = 32
+#
+# ClID = ['PkNL'][0]
+# num_train = 512
+# num_test = 16
+# num_para = 5
+#
+# batch_size = 16
+# num_epochs =  10000 # 20  #200 # 7500 # 200  #110 #50
+# epsilon_mean = 0.0 # 1.0
+# epsilon_std = 1e-6#1e-6 ## original = 1.0, smaller the better 1e-4
+# learning_rate =  1e-5
+# decay_rate = 1.0
+#
+# noise_factor = 0.0 # 0.0 necessary
+# #
+# # ######################## I/O #################################
