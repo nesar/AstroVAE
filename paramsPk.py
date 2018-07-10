cd12957c77294cdb994a3eb54f2b34b772e7dec4
Nesar Ramachandra <nayaknesar@gmail.com>
2018-07-10 09:01:42 -0500

    Pk: dropout

--- paramsPk.py
+++ paramsPk.py
@@ -13,12 +13,12 @@ num_train = 512
 num_test = 32
 num_para = 5
 
-batch_size = 16
-num_epochs =  1000 # 20  #200 # 7500 # 200  #110 #50
+batch_size = 32
+num_epochs =  10000 # 20  #200 # 7500 # 200  #110 #50
 epsilon_mean = 0.0 # 1.0
 epsilon_std = 1e-1#1e-6 ## original = 1.0, smaller the better 1e-4
 learning_rate =  1e-3
-decay_rate = 0.5
+decay_rate = 0.1
 
 noise_factor = 0.0 # 0.0 necessary
 
