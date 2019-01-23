b9012aeac26ea2543328d8c93ee9c6b4577c116c
Nesar Ramachandra <nayaknesar@gmail.com>
2019-01-23 13:36:30 -0600

    new camb generator

--- New_CAMBGen.py
+++ New_CAMBGen.py
@@ -25,7 +25,99 @@ r_{0.05}: [0, 2] \\
 #################### LHC ################
 
 
+''' Latin hypercube design
+https://pythonhosted.org/pyDOE/randomized.html
 
+# /home/nes/MEGA/Google_drive/KU courses/Spring2017/SAMSI_May/AllV/latin.py
+
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+import pyDOE as pyDOE
+
+from scipy.stats.distributions import norm
+
+
+def rescale01(xmin, xmax, f):
+    return (f - xmin) / (xmax - xmin)
+
+
+import SetPub
+
+SetPub.set_pub()
+
+# nsize = 2
+# totalFiles = nsize**5 #32
+totalFiles = 8
+
+np.random.seed(7)
+
+# OmegaM = np.linspace(0.12, 0.155, totalFiles)
+# Omegab = np.linspace(0.0215, 0.0235, totalFiles)
+# # sigma8 = np.linspace(0.7, 0.9, totalFiles)
+# # sigma8 = 0.8*np.ones(shape=totalFiles)
+# sigma8 = np.linspace(0.799, 0.8001, totalFiles)  # Dunno how to set sigma_8 in CAMB yet
+# h = np.linspace(0.55, 0.85, totalFiles)
+# ns = np.linspace(0.85, 1.05, totalFiles)
+
+
+###### NEED TO RECHECK THESE VALUES OMEGAM ~ 0.112
+
+OmegaM = np.linspace(0.10, 0.140, totalFiles)
+Omegab = np.linspace(0.0205, 0.0235, totalFiles)
+sigma8 = np.linspace(0.7, 0.9, totalFiles)
+h = np.linspace(0.55, 0.85, totalFiles)
+ns = np.linspace(0.85, 1.05, totalFiles)
+
+
+
+AllLabels = [r'$\tilde{\Omega}_m$', r'$\tilde{\Omega}_b$', r'$\tilde{\sigma}_8$', r'$\tilde{h}$',
+             r'$\tilde{n}_s$']
+
+AllPara = np.vstack([OmegaM, Omegab, sigma8, h, ns])
+
+lhd = pyDOE.lhs(5, samples=totalFiles, criterion=None) # c cm corr m
+print(lhd)
+print
+# lhd = norm(loc=0, scale=1).ppf(lhd)  # this applies to both factors here
+
+
+##
+f, a = plt.subplots(5, 5, sharex=True, sharey=True)
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+plt.rcParams.update({'font.size': 8})
+
+for i in range(5):
+    for j in range(i+1):
+        print(i,j)
+        # a[i,j].set_xlabel(AllLabels[i])
+        # a[i,j].set_ylabel(AllLabels[j])
+        if(i!=j):
+            a[i, j].scatter(lhd[:, i], lhd[:, j], s=5)
+            a[i, j].grid(True)
+        else:
+            # a[i,i].set_title(AllLabels[i])
+            a[i, i].text(0.4, 0.4, AllLabels[i], size = 'xx-large')
+            hist, bin_edges = np.histogram(lhd[:,i], density=True, bins=64)
+            # a[i,i].bar(hist)
+            a[i,i].bar(bin_edges[:-1], hist/hist.max(), width=0.2)
+            plt.xlim(0,1)
+            plt.ylim(0,1)
+
+            # n, bins, patches = a[i,i].hist(lhd[:,i], bins = 'auto', facecolor='b', alpha=0.25)
+            # a[i, i].plot(lhd[:, i], 'go')
+
+plt.savefig('../Cl_data/Plots/ExtendedLatinSq.png', figsize=(10, 10))
+plt.show()
+idx = (lhd * totalFiles).astype(int)
+
+AllCombinations = np.zeros((totalFiles, 5))
+for i in range(5):
+    AllCombinations[:, i] = AllPara[i][idx[:, i]]
+
+np.savetxt('../Cl_data/Data/ExtendedLatinCosmoP5'+str(totalFiles)+'.txt', AllCombinations)   #### no
+# saving files because the its random everytime
 
 
 
@@ -70,7 +162,7 @@ ell_max = 10000
 # ndim = lmax0 + 1
 
 
-para5 = np.loadtxt('../Cl_data/Data/LatinCosmoP5'+str(totalFiles)+'.txt')
+para5 = np.loadtxt('../Cl_data/Data/ExtendedLatinCosmoP5'+str(totalFiles)+'.txt')
 
 
 
@@ -265,12 +357,12 @@ for i in range(totalFiles):
 ls = np.arange(totCL.shape[0])
 
 # np.save('../Cl_data/Data/LatinPara5P4_'+str(totalFiles)+'.npy', para5)
-np.savetxt('../Cl_data/Data/P5_1ls_'+str(totalFiles)+'.txt', ls)
+np.savetxt('../Cl_data/Data/ExtendedP5_1ls_'+str(totalFiles)+'.txt', ls)
 
-np.savetxt('../Cl_data/Data/P5_2TTCl_'+str(totalFiles)+'.txt', AllTT)
-np.savetxt('../Cl_data/Data/P5_2EECl_'+str(totalFiles)+'.txt', AllEE)
-np.savetxt('../Cl_data/Data/P5_2BBCl_'+str(totalFiles)+'.txt', AllBB)
-np.savetxt('../Cl_data/Data/P5_2TECl_'+str(totalFiles)+'.txt', AllTE)
+np.savetxt('../Cl_data/Data/ExtendedP5_2TTCl_'+str(totalFiles)+'.txt', AllTT)
+np.savetxt('../Cl_data/Data/ExtendedP5_2EECl_'+str(totalFiles)+'.txt', AllEE)
+np.savetxt('../Cl_data/Data/ExtendedP5_2BBCl_'+str(totalFiles)+'.txt', AllBB)
+np.savetxt('../Cl_data/Data/ExtendedP5_2TECl_'+str(totalFiles)+'.txt', AllTE)
 
 time1 = time.time()
 print('camb time:', time1 - time0)
@@ -279,6 +371,16 @@ print('camb time:', time1 - time0)
 plt.figure(32)
 plt.plot(AllEE[:, 7:].T)
 plt.yscale('log')
+plt.xscale('log')
+plt.ylabel(r'$C_l$')
+plt.xlabel('$l$')
+plt.show()
+
+
+
+plt.figure(32)
+plt.plot(AllTE[:, 7:].T)
+# plt.yscale('log')
 plt.xscale('log')
 plt.ylabel(r'$C_l$')
 plt.xlabel('$l$')
