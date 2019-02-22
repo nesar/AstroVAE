9095fed6512cc9593671eab54b55bf7fb5f4ef53
Nesar Ramachandra <nayaknesar@gmail.com>
2019-02-22 12:03:23 -0600

    Checking borders of lhc

--- New_CAMBGen2.py
+++ New_CAMBGen2.py
@@ -220,7 +220,7 @@ AllTE = np.zeros(shape=(totalFiles, num_para + ell_max + 1) ) # Check if this is
 
 newErr = 'STOP SIGINT1: Integration timed out'
 
-for i in range(6, totalFiles):
+for i in range(totalFiles):
 	print(i, para5[i])
 
 	# Set up a new set of parameters for CAMB
