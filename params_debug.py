380b2c0b285fff4793b613c5fbbf9b7ac068ace1
nesar <nayaknesar@gmail.com>
2019-06-27 14:37:38 -0500

    l - dependant rescaling seems to work

--- params_debug.py
+++ params_debug.py
@@ -14,11 +14,11 @@ num_test = 25
 num_para = 5
 
 batch_size = 8
-num_epochs =  5 # 7500 #8000 #7500 # 20  #200 # 7500 # 200  #110 #50
-epsilon_mean = 0.0 # 1.0
+num_epochs =  1000 # 7500 #8000 #7500 # 20  #200 # 7500 # 200  #110 #50
+epsilon_mean = 0.0 #0.0 # 1.0
 epsilon_std = 1e-4 ## original = 1.0, smaller the better 1e-4
-learning_rate =  1e-4
-decay_rate = 1.0
+learning_rate =  1e-4 #1e-4
+decay_rate = 1.0 #1.0
 
 noise_factor = 0.0 # 0.0 necessary
 
@@ -29,7 +29,7 @@ DataDir = MainDir+'Data/'
 PlotsDir = MainDir+'Plots/'
 ModelDir = MainDir+'Model/'
 
-fileOut = 'P'+str(num_para)+'Model_tot' + str(num_train) + '_batch' + str(batch_size) + '_lr' + \
+fileOut = 'P'+str(num_para)+'Model_debug_tot' + str(num_train) + '_batch' + str(batch_size) + '_lr' + \
           str(
     learning_rate) + '_decay' + str(decay_rate) + '_z' + str(latent_dim) + '_epoch' + str(
     num_epochs)
