fbc968b1addc6eb275e41293fe2263b63b52ac3f
nesar <nesar@github.com>
2019-07-30 19:33:00 -0400

    july 30 update

--- params_debug.py
+++ params_debug.py
@@ -14,10 +14,10 @@ num_test = 25
 num_para = 5
 
 batch_size = 32
-num_epochs =  7500 # 7500 #8000 #7500 # 20  #200 # 7500 # 200  #110 #50
+num_epochs =  100 # 7500 #8000 #7500 # 20  #200 # 7500 # 200  #110 #50
 epsilon_mean = 0.0 #0.0 # 1.0
 epsilon_std =  1e-4 #1e-4 ## original = 1.0, smaller the better 1e-4
-learning_rate =  1e-4 #1e-4
+learning_rate =  1e-3 #1e-4
 decay_rate = 1.0 #1.0
 
 noise_factor = 0.0 # 0.0 necessary
